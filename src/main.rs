da2394936f2c7e97daea6533fb54cd6bae218757
Andre Braga Reis <andre@brg.rs>
2025-07-12 11:40:22 +1200

    Remove timestamps from log output

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ async fn main() {
             tracing_subscriber::EnvFilter::try_from_default_env()
                 .unwrap_or_else(|_| "sane_rss=info".into()),
         )
-        .with(tracing_subscriber::fmt::layer())
+        .with(tracing_subscriber::fmt::layer().without_time())
         .init();
 
     info!("Starting sane-rss");
